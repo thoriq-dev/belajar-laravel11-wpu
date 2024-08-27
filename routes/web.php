07e045246c56a3edb9128504967233686d456496
thoriq <thoriqtnf07@gmail.com>
2024-08-27 17:20:30 +0700

    Membuat Blade Component - Seri 4 WPU

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['nama' => 'Thoriq Fikri']);
+});
+
+Route::get('/blog', function () {
+    return view('blog', ['title' => 'Blog']);
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
