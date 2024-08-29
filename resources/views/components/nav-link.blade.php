9a3ac9f5f3211276816ec81a61b559cb818d6fe6
thoriq <thoriqtnf07@gmail.com>
2024-08-29 16:44:10 +0700

    Fix Bugs Navbar Class Active Page 4/4 Done

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,7 +1,16 @@
-@props(['href'])
+{{-- @props(['href'])
 
 <a href="{{ $href }}"
    class="{{ request()->is(trim($href, '/')) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
    aria-current="{{ request()->is(trim($href, '/')) ? 'page' : 'false' }}">
+   {{ $slot }}
+</a> --}}
+
+
+@props(['href', 'active' => false])
+
+<a href="{{ $href }}"
+   class="{{ $active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ $active ? 'page' : 'false' }}">
    {{ $slot }}
 </a>
