59432687ba15e6d71fae187cf446171acaebe249
thoriq <thoriqtnf07@gmail.com>
2024-08-29 10:50:39 +0700

    Memperbaiki 3/4 Bugs pada Navbar, Bugs Active Page

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,5 +1,7 @@
-<a {{ $attributes }}
-    class="{{ request()->is('/') ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
-    aria-current="{{ request()->is('/') ? 'page' : false }}">
-    {{ $slot }}
+@props(['href'])
+
+<a href="{{ $href }}"
+   class="{{ request()->is(trim($href, '/')) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium"
+   aria-current="{{ request()->is(trim($href, '/')) ? 'page' : 'false' }}">
+   {{ $slot }}
 </a>
